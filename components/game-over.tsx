2597e1bc244acee7335efd8688139d57c22814a2
flicstar <felicitybrand@gmail.com>
2025-12-22 20:09:59 +1100

    this time really fix build

--- components/game-over.tsx
+++ components/game-over.tsx
@@ -52,17 +52,7 @@ export default function GameOver(props: Props) {
           <span>{shareText}</span>
           <img src="/images/share.svg" alt="share" className={styles.shareIcon} />
         </Button>
-        <div className={styles.about}>
-      <div>
-        Not in the family? Play{" "}
-        <a
-          href="https://ozonly.pages.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          OzOnly
-        </a>
       </div>
-    </div>
-  </animated.div>
-);
+    </animated.div>
+  );
+}
